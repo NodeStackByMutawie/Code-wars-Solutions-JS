88d1e8112659d2862208a0acceab4d341d835308
ayaabdelsalam-dev <ayaabdelsalam2004.21@gmail.com>
2025-12-28 18:16:42 +0200

    refactor: update UI and fix JavaScript logic

--- EvenOdd/script.js
+++ EvenOdd/script.js
@@ -1,13 +1,41 @@
 
 //  Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
-function EvenOdd(value){
-    // if (value%2 == 0) {
-    //    console.log("even")
-    // }else{
-    //     console.log("odd")
-    // }
+// function EvenOdd(value){
+//     // if (value%2 == 0) {
+//     //    console.log("even")
+//     // }else{
+//     //     console.log("odd")
+//     // }
 
-    return value%2 ==0?"even":"odd";
+//     return value%2 ==0?"even":"odd";
+// }
+// console.log(EvenOdd(7))
+
+// let evenOrOddOptimized = addValue => addValue %2 == 0?"even" : "odd";
+// console.log(evenOrOddOptimized(40));
+
+
+
+function EvenOdd(number) {
+    return number % 2 === 0 ? "even" : "odd";
 }
-console.log(EvenOdd(8))
+
+
+function checkNumber() {
+    let input = document.getElementById("numberInput");
+    let resultDiv = document.getElementById("result");
+    let val = input.value;
+
+  
+    if (val === "") {
+        resultDiv.innerHTML = "Please enter a number first!";
+        return; 
+    }
+
+   
+    let result = EvenOdd(Number(val));
+    
+    
+    resultDiv.innerText = `The number is: ${result}`;
+}
